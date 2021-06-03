5761f7e746329baafece878123db229c46c568d0
Kathy Tavia <kathy.tavia@gmail.com>
2021-06-03 22:18:52 +0200

    finished function to calculate age in days

---
+++ static/js/script.js
@@ -0,0 +1,15 @@
+// Challenge 1
+const ageInDays = () => {
+    const birthYear = prompt("What year were you born?");
+    const ageInDays = (2021 - birthYear) * 365;
+    const h1 = document.createElement('h1');
+    const textAnswer = document.createTextNode(`You are ${ageInDays} days old.`);
+    h1.setAttribute('id', 'ageInDays');
+    h1.appendChild(textAnswer);
+   document.getElementById('flex-box-result').appendChild(h1);
+}
+
+const resetGame = () => {
+    const results = document.getElementById('ageInDays');
+
+}
